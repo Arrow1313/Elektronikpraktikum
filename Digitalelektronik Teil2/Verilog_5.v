ecb2813665eadb62cff3f06865d6d2842b7436f7
Frederik Strothmann <frstrothmann@gmail.com>
2015-01-08 21:00:39 +0100

    anhang fertig

--- Digitalelektronik Teil2/Verilog_5.v
+++ Digitalelektronik Teil2/Verilog_5.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps // Diese Zeitskala (Schrittweite, Auflösung)
+`timescale 1ns / 1ps // Diese Zeitskala (Schrittweite, Aufloesung)
 // ist nur bei Simulationen wichtig
 //////////////////////////////////////////////////////////////////////////////////
 // Company: // Engineer: VERSCHIEDENE KOMMENTARE
@@ -6,28 +6,28 @@
 // ...
 //////////////////////////////////////////////////////////////////////////////////
 module testXC9572( // Jedes Verilog-Programm ist ein Modul mit einem Namen
-   C, CE, CLR, // und in runden Klammern müssen
+   C, CE, CLR, // und in runden Klammern muessen
    segments1, segments2, LED_OUT // alle Ein-/Ausgangssignale genannt werden
    );
 
-input C;                    // Signale als EINgänge definieren
+input C;                    // Signale als EINgaenge definieren
 input CE; input CLR;        // C = Clock, CLR = Clear, jeweils 1 Signal
-output [7:0] segments1;     // Signale als AUSgänge definieren
+output [7:0] segments1;     // Signale als AUSgaenge definieren
 output [7:0] segments2;     // hier haben wir z.B. Signalgruppen von
 output [7:0] LED_OUT;       // 8 Bits, daher die Angabe [7:0]
 
   reg [7:0] segments1;     // Das ist erstmal noch auskommentiert,
-  reg [7:0] segments2;     // weil wir es erst später brauchen.
+  reg [7:0] segments2;     // weil wir es erst spaeter brauchen.
   reg [14:0] prescaler;     // reg = Register, das ist praktisch ein D-Flipflop.
   reg [7:0]  counter;       // Hier haben wir Register mit 15 bzw. 8 Bit.
   reg [7:0] counter2;
 
-assign LED_OUT = counter;   // Der Inhalt des 8-Bit-Zählers "counter" wird
+assign LED_OUT = counter;   // Der Inhalt des 8-Bit-Zaehlers "counter" wird
                             // mit assign direkt den
                             // 8 Bits von LED_OUT zugeordnet.
 
-                                     // Unser Zähler soll mit Signal C getaktet
-always @ (posedge C or posedge CLR)  // und mit CLR zur¨uckgesetzt werden.
+                                     // Unser Zaehler soll mit Signal C getaktet
+always @ (posedge C or posedge CLR)  // und mit CLR zurueckgesetzt werden.
    begin                             // (posedge = ansteigende Flanke)
       if(CLR)                        // Wenn also CLR auf 1 ist ...
          begin
@@ -42,7 +42,7 @@ always @ (posedge C or posedge CLR)  // und mit CLR zur¨uckgesetzt werden.
 						prescaler = prescaler +1;
             if(prescaler == 0)       // und wenn dieser auf 0 ist (das passiert
                begin                 // einmal in 2 hoch 15 Takten!) dann ...
-                  counter = counter + 1; // ... erhöhe den counter um 1
+                  counter = counter + 1; // ... erhoehe den counter um 1
 						if(counter[3:0] == 10)
 							begin
 								counter[3:0] = 0;
@@ -62,19 +62,19 @@ end
 
 // ----  FUNCTION: 7-segment-decoder ------------------------------------
 function [7:0] segment7;             // Eine Funktion mit dem Namen und
-                                     // 8 Bit großen Ausgabewert segment7
+                                     // 8 Bit grossen Ausgabewert segment7
 
-   input [3:0] decodeinput;          // Was an die Funktion übergeben wird,
+   input [3:0] decodeinput;          // Was an die Funktion uebergeben wird,
                                      // wird innerhalb der Funktion unter dem
-                                     // Namen decodeinput geführt (4 Bit).
+                                     // Namen decodeinput gefuehrt (4 Bit).
 
-   case (decodeinput)                // Für den Fall, daß decodeinput ...
+   case (decodeinput)                // Fuer den Fall, dass decodeinput ...
       4'h0 : segment7 = 8'b00111111; // ... den Wert 0 hat: segment7 = Muster "0"
-                        // Dgfedcba <<< Stellen f¨ur Dezimalpunkt (D) und 7 Segmente
-                        // als 8-Bit-Binärzahl (daher 8'b)
+                        // Dgfedcba <<< Stellen fuer Dezimalpunkt (D) und 7 Segmente
+                        // als 8-Bit-Binaerzahl (daher 8'b)
                         // decodeinput schreiben wir als 4-Bit-Hexadezimalzahl, also 0-9,a-f
       4'h1 : segment7 = 8'b00000110; // das Strichmuster "1"
-      4'h2 : segment7 = 8'b01011011; // diese Strichmuster müssen Sie noch ändern
+      4'h2 : segment7 = 8'b01011011; // diese Strichmuster muessen Sie noch aendern
       4'h3 : segment7 = 8'b01001111;
       4'h4 : segment7 = 8'b01100110;
       4'h5 : segment7 = 8'b01101101;
